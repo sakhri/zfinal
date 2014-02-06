e02b04c8865ea3ce968eb48f528e9987378992b3
sakhri <medaminesakhri@gmail.com>
2014-02-06 01:14:24 +0100

    first commit

---
+++ ztestgit1/src/ztestgit1/final1.java
@@ -0,0 +1,12 @@
+package ztestgit1;
+
+public class final1 {
+
+	
+	
+	public void afficher()
+	{
+		System.out.println("premier test premier fichier");
+	}
+
+}
---
+++ ztestgit1/src/ztestgit1/final2.java
@@ -0,0 +1,11 @@
+package ztestgit1;
+
+public class final2 {
+
+	
+public void afficher()
+{
+	System.out.println("premier test deuxiéme fichier");
+}
+	
+}
